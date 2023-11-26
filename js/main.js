c791d157bf349c2026862990239bba3b37a7dea6
RSkooTy <127528782+RSkooTy@users.noreply.github.com>
2023-11-26 23:13:40 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,46 +1,21 @@
-import {setFormValue, submitSignUpForm, validateEmail, validatePassword} from "./utils.js"
-
-
-////// ДЕМОНСТРАЦИОННЫЙ УЧАСТОК КОДА. На оценку не влияет, исключительно для саморазвития.
-
-// Предлагаю "поиграться" с частями кода ниже, чтобы познакомиться с JS
-// Получаем элемент и меняем его класс, который определеён в библиотеке стилей materialize
-const password = document.getElementById('password');
-password.classList.add("valid")
-password.classList.remove("valid")
-
-// В браузере можно посмотреть, что из себя представляет документ
-// (CTRL+SHIFT+i для открытия консоли и открыть вкладку "консоль", туда будет залогированно значение)
-console.log("Document")
-console.log(document)
-
-// Если запросить id, которого нет в DOM дереве - вернется undefined
-// => надо быть осторожней: коллега может поменять id вашего элемента и упадёт !ВАШ! код
-// const first_name = document.getElementById('first_name_invalid');
-// first_name.oninput = (e) => validatePassword(e)
-
-// Селекция по классу. Может пригодится, для того, чтобы упростить обработку полей в двух формах.
-// Чтобы не делать кучу уникальных айди, можно определённым полям формы давать один класс и обрабатывать их в цикле
-// const passwords = document.querySelectorAll('.password')
-// console.log(passwords)
-// for (const password of passwords) {
-//   password.style.background = "red"
-// }
-
-////// КОНЕЦ ДЕМОНСТРАЦИОННОГО УЧАСТКА КОДА. Дальше код для оцениваемой части задания
-
+import {setFormValue, submitSignUpForm, validateEmail, validatePassword, getValidationStatus, validatePasswordRepeat, validateEmpty, submitSignInForm} from "./utils.js"
 
 // Выписываем все айдишники HTMl-элементов в константы для переиспользования
 const first_name_id = 'first_name'
 const last_name_id = 'last_name'
 const password_id = 'password'
 const email_id = 'email'
+const password_repeat_id = 'password-repeat'
 
 const sign_in_link_id = 'sign_in_link'
 const sign_up_form_id = 'sign_up_form'
-// const sign_in_form_id = 'sign_in_form'  // Пригодится
+const sign_in_btn_id = 'log_in_btn'
 const sign_up_btn_id = 'sign_up_btn'
 const sign_in_form_id = 'sign_in_form'
+const sign_in_email_id = 'sign_in_email'
+const sign_in_password_id = 'sign_in_password'
+const return_btn_id = 'return_btn'
+
 
 
 // Получаем элемент DOM-дерева по id и присваиваем значение аттрибуту oninput
@@ -49,11 +24,28 @@ const sign_in_form_id = 'sign_in_form'
 // Гуглить по тегам "события JS", "onchange/oninput HTML", "стрелочные функции JS", ...
 
 const first_name = document.getElementById(first_name_id);
-first_name.oninput = (e) => setFormValue(first_name_id, e.target.value)  // Установить значение без валидации
+first_name.oninput = (e) => setFormValue(first_name_id, e.target.value, validateEmpty)
+
+const last_name = document.getElementById(last_name_id);
+last_name.oninput = (e) => setFormValue(last_name_id, e.target.value, validateEmpty)
 
 const email = document.getElementById(email_id);
-email.oninput = (e) => setFormValue(email_id, e.target.value, validateEmail) // Установить значение с валидацией
+email.oninput = (e) => setFormValue(email_id, e.target.value, validateEmail)
 
+const password = document.getElementById(password_id)
+password.oninput = (e) => {
+    setFormValue(password_id, e.target.value, validatePassword);
+    password_repeat.value ? setFormValue(password_repeat_id, password_repeat.value, validatePasswordRepeat) : () => {};
+}
+
+const password_repeat = document.getElementById(password_repeat_id)
+password_repeat.oninput = (e) => setFormValue(password_repeat_id, e.target.value, validatePasswordRepeat)
+
+const sign_in_email = document.getElementById(sign_in_email_id)
+sign_in_email.oninput = (e) => setFormValue(sign_in_email_id, e.target.value, validateEmail)
+
+const sign_in_password = document.getElementById(sign_in_password_id)
+sign_in_password.oninput = (e) => setFormValue(sign_in_password_id, e.target.value, validatePassword)
 
 
 // Меняем стили объекта DOM дерева. Это позволяет скрыть форму регистрации и показать форму авторизации
@@ -62,6 +54,12 @@ const switch_to_sign_in = document.getElementById(sign_in_link_id);
 switch_to_sign_in.onclick = (e) => {
   document.getElementById(sign_up_form_id).style.display = "none"
   document.getElementById(sign_in_form_id).style.display = ""
+}
+
+const return_btn = document.getElementById(return_btn_id);
+return_btn.onclick = (e) => {
+  document.getElementById(sign_up_form_id).style.display = ""
+  document.getElementById(sign_in_form_id).style.display = "none"
 }
 
 
@@ -73,3 +71,48 @@ sign_up_btn.onclick = (e) => {
   submitSignUpForm()
 }
 
+const sign_up_inputs = document.querySelectorAll('.sign-up')
+console.log(sign_up_inputs)
+
+const updateSignUpButtonState = () => {
+    sign_up_btn.disabled = !getValidationStatus();
+}
+
+const handleInput = (field) => {
+    setFormValue(field.id, field.value, validateEmpty);
+    updateSignUpButtonState();
+}
+
+sign_up_inputs.forEach((field) => {
+    field.oniput = (e) => handleInput(field);
+})
+
+updateSignUpButtonState();
+
+const sign_in_btn = document.getElementById(sign_in_btn_id)
+sign_in_btn.onclick = (e) =>{
+  e.preventDefault()
+  submitSignUpForm(email_in_id, password_in_id)
+}
+
+const sign_in_inputs = document.querySelectorAll('.sign-in')
+const signInValues = {}
+
+const updateSignInValue = (field) =>{
+    signInValues[field.id] = field.value;
+    setFormValue(field.id, field. value);
+    sign_in_btn.disabled = !Object.values(signInValues).every((val) => String(val).length > 0);
+};
+
+const setSignInInput = (field) => {
+    field.oniput = (e) => {
+        updateSignInValue(field);
+    };
+};
+
+sign_in_inputs.forEach((field) => {
+    updateSignInValue(field);
+    setSignInInput(field);
+});
+
+sign_in_btn.disabled = !getValidationStatus();
--- js/utils.js
+++ js/utils.js
@@ -1,17 +1,20 @@
-const formValues = {}  // Сюда пишутся значения формы (Object как в Java, или dict из Python)
-const formValidation = {}  // Сюда пишутся статусы валидации каждого поля. Если поле ни разу не валидировалось,
+export const formValues = {}  // Сюда пишутся значения формы (Object как в Java, или dict из Python)
+export const formValidation = {}  // Сюда пишутся статусы валидации каждого поля. Если поле ни разу не валидировалось,
 // то при обращении к Object вернётся undefined, который при логическом сравнении обрабатывается как false
 
 
 // Объявляется и инициализируется константная переменная
 // Инициализация функцией, заданной в стрелочном виде
-export const validatePassword = (e) => {
-  formValidation.password = e.target.value
-  console.log("Password validation...")
-  console.log(e)
+export const validatePassword = (password) => {
   // Напишите код валидации здесь и присвойте true/false в объект(словарь) formValidation
   // formValidation.password = ...  // formValidation['password'] = ... - то же самое, но другой синтаксис
-  return formValidation.password !== undefined   // Это заглушка, return вероятно надо переписать
+  const regExp = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+  return String(password).match(regExp);
+}
+
+
+export const validatePasswordRepeat = (repeat) => {
+  return String(formValues.password) == String(repeat) && String(repeat).length > 0 && validatePassword(repeat);
 }
 
 
@@ -34,14 +37,30 @@ export const getValidationStatus = () => {
   return Object.values(formValidation).every((validationStatus) => !!validationStatus)
 }
 
+export const validateEmpty = (input) =>
+{
+  return input.length > 0;
+}
+
+export const getFieldValidationStatus = (id) =>{
+  return formValidation.id
+}
 
 // Функция возвращающая которая ставит значение поля в форме по ключу
 export const setFormValue = (valueKey, newValue, validator) => {
   formValues[valueKey] = newValue
+
   if (validator !== undefined) {
-    formValidation[valueKey] = validator(newValue)
+    const isValid = validator(newValue);
+    formValidation[valueKey] = isValid;
+
+    const form = document.getElementById(valueKey);
+    form.classList.remove('valid', 'invalid')
+
+    if (String(newValue).length > 0)
+      form.classList.add(isValid ? 'valid' : 'invalid');
   }
-}
+};
 
 
 // Функция для обработки отправки формы регистрации
@@ -54,4 +73,10 @@ export const submitSignUpForm = () => {
   console.log("FORM IS FINE")
   console.log(formValues)
   return true
+}
+
+export const submitSignInForm = (sign_in_email_id, sign_in_password_id) => {
+  console.log(formValues)
+  console.log('Email: ${formValues[sign_in_email_id]}')
+  console.log('Password ${formValues[sign_in_password_id]}')
 }
